3906449894bfbf9901058ff52b491396e68a708f
Jihun <110877415+hjh3229@users.noreply.github.com>
2023-05-18 11:15:06 +0900

    member desc
    update member desc function with script.js and app.py

--- 한지훈/app.py
+++ 한지훈/app.py
@@ -1,49 +1,75 @@
-from flask import Flask, render_template, request, jsonify
+##한지훈, 송이삭, 김희열
+from flask import Flask, render_template, request, jsonify, url_for #(송어진) url_for는 JS,CSS 외부파일로 분리를 위해
 app = Flask(__name__)
 
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-client = MongoClient('mongodb+srv://sparta:test@cluster0.oid4wb8.mongodb.net/?retryWrites=true&w=majority')
-database = client['dbsparta']
-collection = database['mbti_data']
+client = MongoClient('mongodb+srv://sparta:test@cluster0.gcgk5vj.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 데이터베이스 값 초기화
-mbti_values = {
-       'e' : 0,
-       'i' : 0,
-       'n' : 0,
-       's' : 0,
-       't' : 0,
-       'f' : 0,
-       'j' : 0,
-       'p' : 0
-}
+##김희열
+@app.route("/mbti", methods=["POST"])
+def mbti_post():
+    IE = request.form['IE_give']
+    NS = request.form['NS_give']
+    TF = request.form['TF_give']
+    PJ = request.form['PJ_give']
+    
+    doc = {
+        'IE' : IE,
+        'NS' : NS,
+        'TF' : TF,
+        'PJ' : PJ
+    }
+    
+    db.mbti.insert_one(doc)
+    
+    return jsonify({'msg': '저장 완료'})
+    
+def postMbti():
+    mbti_receive = request.form["mbti_give"]
+    mbti_receive = mbti_receive.replace(",", "")  # ,가 섞인 문자열로 받아와지기 때문에 이를 제거
+    doc = {"mbti": mbti_receive}
+    db.mbti.insert_one(doc)
+    print(mbti_receive, len(mbti_receive), type(mbti_receive))
 
-@app.route('/submit', methods=['POST']) # 제출 버튼을 눌렀을 때 mbti값을 저장하기 위한 post
-def submit():
-    # 클라이언트로부터 전송된 데이터를 받습니다.
-    mbti_data = request.get_json()
+    mbtiCnt = list(db.mbtiCnt.find({}))
+    if len(mbtiCnt) == 0:
+        mbtiCnt = {
+            "id": 0,
+            "e": 0,
+            "i": 0,
+            "n": 0,
+            "s": 0,
+            "t": 0,
+            "f": 0,
+            "j": 0,
+            "p": 0,
+        }
+    else:
+        mbtiCnt = mbtiCnt[0]
+        del mbtiCnt["_id"]
 
-    # mbti_data를 사용하여 mbti_values를 업데이트합니다.
-    for mbti_type, value in mbti_data.items():
-        mbti_values[mbti_type] += value
+    for a in mbti_receive:
+        mbtiCnt[a] = mbtiCnt[a] + 1
+    db.mbtiCnt.delete_one({"id": 0})
+    doc = mbtiCnt
+    db.mbtiCnt.insert_one(doc)
 
-    # 업데이트된 mbti_values를 데이터베이스에 저장하는 코드를 추가해야 합니다.
-    document_id = ObjectId('646361d2994e4ffc16c1cf03')
-    collection.update_one({'_id':document_id}, {'$set': mbti_values})
-    return jsonify({'msg': '저장완료'})
+    return jsonify({"msg": "저장완료!"})
 
-@app.route("/statistics", methods=["GET"]) # 메인화면에서 mbti 통계를 불러오기 위한 get
-def movies1():
-    document_id = ObjectId('646361d2994e4ffc16c1cf03')
-    # mbti_values 불러오기
-    mbti_statistics = list(collection.find({'_id':document_id},{'_id':False}))
-    return jsonify({'result': mbti_statistics}) # 저장한 값 반환
+@app.route("/mbti", methods=["GET"])
+def mbti_get():
+    all_mbti = list(db.mbti.find({},{'_id':False}))
+    return jsonify({'result': all_mbti})
+
+@app.route("/member", methods=["GET"])
+def member_get():
+    member_desc = list(db.member.find({},{'_id':False}))
+    return jsonify({'result': member_desc})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
